580ff63172d93cfb80e3de4cb954453b52a99bd0
Jaclyn Mathai <jacburge@gmail.com>
2025-08-08 19:46:49 -0400

    correct asset paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nfl-survivor-pool/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
